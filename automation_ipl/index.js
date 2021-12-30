3820dfd1fbb0104a015fa3f6d0dc6071f7daaaeb
battleplayer02 <himanshu00qwerty@gmail.com>
2021-12-30 17:35:37 +0530

    a

--- automation_ipl/index.js
+++ automation_ipl/index.js
@@ -21,7 +21,7 @@ console.log("start");
                 let data = await page.evaluate(() => {
                     let team1 = {};
                     let team2 = {};
-                    team1.name = document.querySelectorAll(".event  div.teams p.name")[0].innerText;
+                    team1.name = document.querySelectorAll(".event div.teams p.name")[0].innerText;
                     team2.name = document.querySelectorAll(".event  div.teams p.name")[1].innerText;
                     console.log(team1.name);
                     console.log(team2.name);
