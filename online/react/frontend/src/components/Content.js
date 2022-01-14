98f9ccf7794d063a199a5235e6548a80e32fc6db
battleplayer02 <himanshu00qwerty@gmail.com>
2022-01-14 14:31:28 +0530

    react

--- online/react/frontend/src/components/Content.js
+++ online/react/frontend/src/components/Content.js
@@ -1,17 +1,23 @@
 import React from "react"
+import data from "../data"
 import Product from "./Product"
 
 export default function Content() {
     return (
         <div className="content">
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
+            {
+                data.products.map(product =>
+                    <Product
+                        key={product._id}
+                        name={product.name}
+                        category={product.category}
+                        brand={product.brand}
+                        price={product.price}
+                        image={product.image}
+                    />
+                )
+            }
         </div>
     )
 }
+
--- online/react/frontend/src/components/Product.js
+++ online/react/frontend/src/components/Product.js
@@ -1,23 +1,24 @@
 import React from 'react'
-import image from "../images/logo512.png"
-export default function Product() {
+export default function Product({ name, category, brand, price, image }) {
     return (
         <div className='product'>
-            <div className="header">
-                <img className="img" src={image} height="100px" width="50px"/>
+            <div className="product-header">
+                <img className="img" src={image}
+                    alt='product' />
             </div>
             <div className="content-product">
                 <div className="title">
                     <div className='details'>
                         <h3>
-                            Product Title
+                            {name}
                         </h3>
-                        <div>Category</div>
+                        <div>Category : {category}</div>
+                        <div>Barand: {brand}</div>
                     </div>
                     <div className='wrapper'>
                         <div className='price'>
                             <span>Rs.</span>
-                            <span>1000</span>
+                            <span>{price}</span>
                         </div>
                         <div className="btn">
                             <button>Add To Cart</button>
