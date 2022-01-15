798af5ebb2d218565104bd4f0a20d400c6d5dea0
battleplayer02 <himanshu00qwerty@gmail.com>
2022-01-15 09:36:09 +0530

    darabase

---
+++ online/react/postgres-nodejs-connect/index.js
@@ -0,0 +1,25 @@
+let pg = require("pg");
+let config = {
+    user: "uw8j9xywyopeonb9cwon",
+    database: "bk0hqc7dq1flwq4i2n6v",
+    password: "ZsM6RiKLmc7scgZFi2Nb",
+    port: 5432,
+    host: "bk0hqc7dq1flwq4i2n6v-postgresql.services.clever-cloud.com",
+    max: 10,
+    idleTimeoutMillis: 30000
+};
+let pool = new pg.Pool(config);
+pool.on("error", function (err, client) {
+    console.error("idle client error", err.message, err.stack);
+});
+pool.connect(function (err, client, done) {
+    if (err) {
+        return console.error("error fetching client from pool", err);
+    }
+});
+pool.query("SELECT * FROM student", function (err, result) {
+    if (err) {
+        return console.error("error running query", err);
+    }
+    console.log(result.rows);
+});
