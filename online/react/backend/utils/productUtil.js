755255878543952b4f6f92ae7c48a841c0e625c1
battleplayer02 <himanshu00qwerty@gmail.com>
2022-02-05 12:54:10 +0530

    db

--- online/react/backend/utils/productUtil.js
+++ online/react/backend/utils/productUtil.js
@@ -8,3 +8,23 @@ module.exports.getAllProducts = async function () {
         return error;
     }
 }
+
+
+module.exports.getCategories = async function () {
+    try {
+        let result = await client
+            .query("select category from products group by category");
+        return result.rows;
+    } catch (error) {
+        return error;
+    }
+}
+
+module.exports.getSingleProduct = async function (id) {
+    try {
+        let result = await client.query(`select * from products where _id = ${id}`);
+        return result.rows[0];
+    } catch (error) {
+        return error;
+    }
+}
