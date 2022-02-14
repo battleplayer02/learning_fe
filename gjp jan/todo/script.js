e51196558e3ddbf87d8f23a7957afe82e1a5dab5
battleplayer02 <himanshu00qwerty@gmail.com>
2022-02-14 21:28:45 +0530

    edit

--- gjp jan/todo/script.js
+++ gjp jan/todo/script.js
@@ -5,7 +5,7 @@ const todoContainer = document.getElementById("todo_container");
 const error = document.querySelector(".error");
 const title = document.getElementById("title");
 const todo__text = document.getElementById("todo__text");
-
+let editMode = -1;
 let counter = 0;
 
 // get the todos from local storage and display them
@@ -16,7 +16,9 @@ counter = data.length;
 for (let i = 0; i < data.length; i++) {
     // add the i index aas id
     let todotext = `<div class="todo-list" todo-id='${i}'>
-                        <div class="todo-header">${data[i].title}
+                        <div class="todo-header">
+                            <span id='todo-header-text'>${data[i].title}</span>
+                            <i class="fa fa-edit" style="font-size:24px;margin-right:20px"></i>
                             <button>X</button>
                         </div>
                         <div class="todo-text">${data[i].text}</div>
@@ -34,20 +36,44 @@ addBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
     dialogBox.style.display = "none";
     error.classList.add("gayab");
+    todo__text.value = "";
+    title.value = "";
 });
 
 function addTodo() {
     if (todo__text.value !== "" && title.value !== "") {
-        let todotext = `<div class="todo-list" todo-id='${counter}'>
-                            <div class="todo-header">${title.value}
-                                <button>X</button>
-                            </div>
-                        <div class="todo-text">${todo__text.value}</div>
-                        </div>`;
-        todoContainer.innerHTML = todoContainer.innerHTML + todotext;
-        deletelisteners();
         // GET the old data [] from local storage
         let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+        if (editMode !== -1) {
+            // update the old data
+            for (let i = 0; i < todos.length; i++) {
+                if (todos[i].id == editMode) {
+                    todos[i].title = title.value;
+                    todos[i].text = todo__text.value;
+                }
+            }
+            // set the new data to local storage
+            localStorage.setItem("todos", JSON.stringify(todos));
+
+            // set it on the view
+            let todo = document.querySelector(`[todo-id='${editMode}']`);
+            todo.querySelector("#todo-header-text").innerText = title.value;
+            todo.querySelector(".todo-text").innerText = todo__text.value;
+            editMode = -1;
+            dialogBox.style.display = "none";
+            return;
+        }
+        let todotext = `<div class="todo-list" todo-id='${counter}'>
+                            <div class="todo-header">
+                                <span id='todo-header-text'>${title.value}</span>
+                                <i class="fa fa-edit" style="font-size:24px;margin-right:20px"></i>
+                                <button>X</button>
+                            </div>
+                            <div class="todo-text">${todo__text.value}</div>
+                        </div>`;
+        todoContainer.innerHTML += todotext;
+        deletelisteners();
         // add the new todo to the old data
         todos.push({
             id: counter,
@@ -69,10 +95,12 @@ function addTodo() {
 
 function deletelisteners() {
     const delarr = document.querySelectorAll(".todo-header button");
+    const editarr = document.querySelectorAll(".todo-header i");
+    console.log(editarr);
     for (let i = 0; i < delarr.length; i++) {
         delarr[i].addEventListener("click", function (e) {
             let todo = e.target.closest(".todo-list");
-            console.log(todo);
+            console.log(todo)
             let id = todo.getAttribute("todo-id");
             console.log(id);
             let data = JSON.parse(localStorage.getItem("todos"));
@@ -82,9 +110,21 @@ function deletelisteners() {
                     newData.push(data[i]);
                 }
             }
-            console.log(newData);
             localStorage.setItem("todos", JSON.stringify(newData));
             todo.classList.add("gayab");
+        });
+
+        editarr[i].addEventListener("click", function (e) {
+            dialogBox.style.display = "flex";
+            let todo = e.target.closest(".todo-list");
+            let id = todo.getAttribute("todo-id");
+            editMode = id;
+            console.log(todo)
+            // first way 
+            //  get the data of title and desc from the todo
+            title.value = todo.querySelector("#todo-header-text").innerText;
+            todo__text.value = todo.querySelector(".todo-text").innerText;
+            //  get the data of title and desc from the local storage
         });
     }
 }
