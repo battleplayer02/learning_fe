cc55cbee5870c77d886bb00dd21d2c2b4f0c970f
Snehal <snehal9140@gmail.com>
2022-02-14 11:51:22 +0530

    redux-counter

--- online/redux/reduxcounter/src/App.js
+++ online/redux/reduxcounter/src/App.js
@@ -1,15 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import { useDispatch, useSelector } from "react-redux";
+import { incNumber } from './actions/action';
+import {useDispatch,useSelector} from "react-redux"
 
 
 function App() {
+  let count = useSelector((state) => state.ChangeNum)
   const dispatch = useDispatch();
-  useSelector((ele)=>console.log(ele))
+  function handleInc() {
+    console.log("increment call");
+    dispatch(incNumber())
+  }
   return (
     <>
-      <button>+</button>
-        0
+      <button onClick={handleInc}>+</button>
+        {count}
       <button>-</button>
     </>
   );
--- online/redux/reduxcounter/src/reducers/reducer.js
+++ online/redux/reduxcounter/src/reducers/reducer.js
@@ -1,8 +1,13 @@
-const initialState = 0;
+const initialState = 10;
 const ChangeNum = (state = initialState, action) => {
     switch (action.type) {
-        case "increment": state + 1;
-        default: state;
+        case "increment": return state + 1;
+        default: return state;
     }
+    // if (action.type == "increment") {
+    //     return state + 1;
+    // } else {
+    //     return state;
+    // }
 }
 export default ChangeNum;
